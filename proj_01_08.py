e004d0a5cd97e5c2a6e70b64e617b781229e58a0
James Fraser <jfraser.f17@warren-wilson.edu>
2020-01-30 16:01:38 -0500

    messed a bit with this one. don't know if it still works

--- proj_01_08.py
+++ proj_01_08.py
@@ -64,6 +64,13 @@ if matches:
     print("Match of digit part is: ", matches[1])
 """
 
+'''1) Regular Expressions
+Look at proj_01_08.py.  If you execute that in the console, it defines some variables.
+Put a multiline comment in your Thursday.py for this section (start with """ and end with """)
+Copy paste these instructions in there.  (copy out everything highlighted)
+Look through the "links" variable and see if you can find some image covers.
+Edit Proj_01_08 to make the loop in proj_01_08 print out the path to the images for all of the book cover thumbs.
+'''
 
 
 # find a random set of gutenberg texts in english
@@ -86,4 +93,21 @@ for link in links:
         print("no match: ", test_str)
 
 booknums
+'''
 
+
+links = soup.find_all("img")
+# Go through and build a list of the book numbers out of the matching links.
+booknums = []
+for link in links:
+    regex = r"src.*jpg"
+    test_str = link.get('src')
+    matches = re.match(regex, test_str, re.IGNORECASE)
+    if matches:
+        print(f"Matched: {matches[1]} - {test_str}")
+        booknums.append(matches[1])
+    else:
+        print("no match: ", test_str)
+
+booknums
+'''
